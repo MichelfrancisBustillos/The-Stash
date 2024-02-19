b5c4e526d35aa2b9a7ade3617177d28e94198553
MichelfrancisBustillos <michelfrancisb@gmail.com>
2024-02-19 15:55:54 -0600

    Update & Rename Script

--- UnattendedUpdates&NotificationsSetup.sh
+++ UnattendedUpdates&NotificationsSetup.sh
@@ -7,10 +7,13 @@ dpkg-reconfigure --priority=low unattended-upgrades
 systemctl enable unattended-upgrades
 systemctl start unattended-upgrades
 
-apt install libsasl2-mobules mailutils
+apt install libsasl2-modules mailutils -y
 curl -O https://raw.githubusercontent.com/MichelfrancisBustillos/The-Stash/master/LinuxConfigFiles/main.cf
 cp main.cf /etc/postfix
 echo "smtp.gmail.com michelfrancisb@gmail.com:siiolmcnufubpqnm" > /etc/postfix/sasl_passwd
 chmod 600 /etc/postfix/sasl_passwd
-postmap hash:sasl_passwd
+postmap hash:/etc/postfix/sasl_passwd
 postfix reload
+
+rm 50unattended-upgrades
+rm main.cf
